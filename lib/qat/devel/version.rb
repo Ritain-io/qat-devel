9cce82000f544da910c12b70b6b2a51c746cade1
jesus <pedro.jesus@readinessit.com>
2019-08-01 11:47:34 -0400

    Gem version should be 6.0.1

--- lib/qat/devel/version.rb
+++ lib/qat/devel/version.rb
@@ -1,10 +1,9 @@
 # QAT namespace
 module QAT
   # Namespace for QAT Devel
-  #@since 1.0.0
+  #@since 6.0.0
   module Devel
     # Represents QAT Devel version
-    #VERSION = '2.2.2'
     VERSION = '6.0.1'
   end
 end
